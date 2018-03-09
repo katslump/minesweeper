a0fc71c3bbd1c9e3a1b66acdc8ddd7ac766da655
katslump <kat.slump@gmail.com>
2018-03-08 18:49:11 -0800

    Front-end added

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
 let board = [];
-const numRows = 3;
-const numCols = 3;
-
-function Player(name, turn) {
-    this.name = name;
-    this.turn = turn;
-}
-
-function Board(width, height, grid) {
-    this.width = width;
-    this.height = height;
-    this.grid = [];
-}
-
-function Mine(x, y, active) {
-    this.x = x;
-    this.y = y;
-    this.active = active;
-}
-
-function Field(x, y, revealed) {
-    this.x = x;
-    this.y = y;
-    this.revealed = revealed;
-}
+const numRows = 10;
+const numCols = 10;
 
 function makeBoard() {
     board = new Array(numRows);
@@ -33,10 +10,28 @@ function makeBoard() {
 }
 
 function fillBoard() {
+    let count = 0;
     for (let i = 0; i < numRows; i++) {
         for (let j = 0; j < numCols; j++) {
             board[i][j] = '-';
+            $("#grid").append('<a href="#" id=' + count + ' class="btn btn-sq-xs btn-primary" onclick="handleEvent(' + count + ',' + i + ',' + j + ')"></a>');
+            count++;
         }
+        $("#grid").append('<br/>');
+    }
+}
+
+function handleEvent(count, row, col) {
+    let element = document.getElementById(count);
+
+    if (checkForMine(count, row, col)) {
+        element.className = "btn btn-sq-xs btn-danger";
+    } else {
+        let mineCount = getMineCount(count, row, col);
+        if (mineCount !== 0) {
+            element.textContent = mineCount.toString();
+        }
+        element.className = "btn btn-sq-xs btn-success";
     }
 }
 
@@ -54,93 +49,89 @@ function placeMines(numOfMines) {
         console.log("Mine will be at row: " + randomRow + " col: " + randomCol);
         board[randomRow][randomCol] = '*';
     }
-    checkForMine(Math.floor(Math.random() * numRows),Math.floor(Math.random() * numCols));
 
 }
 
-function checkForMine(x, y) {
-    console.log("Checking for a mine at: " + x + ', ' + y);
+function checkForMine(count, x, y) {
     if (board[x][y] === '*') {
-        console.log("MINE FOUND AT: " + x + " " + y);
-    } else if (checkRow(x, y) || checkCol(x, y) || checkDiagonals(x, y)) {
-        console.log("MINE FOUND AT: " + x + " " + y);
+        return true;
     } else {
-        console.log("NO // MINE FOUND AT: " + x + " " + y);
+        return false;
     }
 }
 
-function checkRow(x, y) {
+function getMineCount(count, x, y) {
+    return checkRow(count, x, y) + checkCol(count, x, y) + checkDiagonals(count, x, y);
+}
 
-    if(x-1 >= 0 && x + 1 < numRows) {
-        // Check right
+function checkRow(count, x, y) {
+    let mineCount = 0;
+
+    if (x - 1 >= 0) {
+        if (board[x - 1][y] === '*') {
+            mineCount++;
+        }
+    }
+
+    if (x + 1 < numRows) {
         if (board[x + 1][y] === '*') {
-            return true;
-            // Check left
-        } else if (0 && board[x - 1][y] === '*' ) {
-            return true;
-            // Not found via row
-        } else {
-            return false;
+            mineCount++;
         }
-    } else {
-        return false;
     }
 
-
+    return mineCount;
 }
 
-function checkCol(x, y) {
+function checkCol(count, x, y) {
+    let mineCount = 0;
 
-    if(y-1 >= 0 && y + 1 < numCols) {
-        // Check above
-        if (board[x][y + 1] === "*" && board[x][y+1].length > 0) {
-            return true;
-            // Check below
-        } else if (board[x][y - 1] === "*" && board[x][y-1].length > 0) {
-            return true;
-            // Not found via column
-        } else {
-            return false;
+    if (y - 1 >= 0) {
+        if (board[x][y + 1] === "*") {
+            mineCount++;
         }
-    } else {
-        return false;
     }
+
+    if (y + 1 < numCols) {
+        if (board[x][y - 1] === "*") {
+            mineCount++;
+        }
+    }
+
+    return mineCount;
 }
 
-function checkDiagonals(x, y) {
-
-    if(x-1 < 0 || y-1 < 0) {
-        console.log("Out of bounds for left and bottom");
-    } else if (x+1 >= numRows  || y+1 < numCols) {
-        console.log("Out of bounds for right and top");
-    } else {
-        // Check top right diagonal
+function checkDiagonals(count, x, y) {
+    let mineCount = 0;
+    if (x + 1 <= numRows - 1 && y + 1 < numCols) {
         if (board[x + 1][y + 1] === "*") {
-            return true;
-            // Check top left diagonal
-        } else if (board[x - 1][y + 1] === "*") {
-            return true;
-            // Check bottom right diagonal
-        } else if (board[x + 1][y - 1] === "*") {
-            return true;
-            // Check bottom left diagonal
-        } else if (board[x - 1][y - 1] === "*") {
-            return true;
-            // Not found via diagonals
-        } else {
-            return false;
-
+            mineCount++;
         }
     }
-
+    if (x - 1 >= 0 && y - 1 >= 0) {
+        if (board[x - 1][y - 1] === "*") {
+            mineCount++;
+        }
+    }
+    if (y + 1 <= numCols - 1 && x - 1 >= 0) {
+        if (board[x - 1][y + 1] === "*") {
+            mineCount++;
+        }
+    }
+    if (x + 1 <= numRows - 1 && y - 1 >= 0) {
+        if (board[x + 1][y - 1] === "*") {
+            mineCount++;
+        }
+    }
+    return mineCount;
 }
 
 function init() {
     makeBoard();
     fillBoard();
-    placeMines(1);
+    placeMines(10);
 }
 
 $(document).ready(function() {
     init();
+
 });
